d2c7d429c7f17285a974a955e30733724382c65d
Fenrnado Mousinho <fmousinho76@gmail.com>
2025-10-09 18:36:51 -0500

    fixed loading

--- app/(dashboard)/dataprep/playerView.tsx
+++ app/(dashboard)/dataprep/playerView.tsx
@@ -89,17 +89,15 @@ export default function PlayerCrop({
 
   return (
     <div className="relative group flex-shrink-0" style={{ width: containerWidth }}>
-      {!loading && (
-        <img
-          ref={handleImageRef}
-          src={src}
-          alt="data"
-          className="object-contain rounded border flex-shrink-0"
-          style={{ height, width: containerWidth }}
-          onLoad={handleImageLoad}
-          onError={handleImageError}
-        />
-      )}
+      <img
+        ref={handleImageRef}
+        src={src}
+        alt="data"
+        className={`object-contain rounded border flex-shrink-0 ${loading ? 'opacity-0 absolute inset-0' : ''}`}
+        style={{ height, width: containerWidth }}
+        onLoad={handleImageLoad}
+        onError={handleImageError}
+      />
       {loading && (
         <div
           className="absolute inset-0 rounded border bg-gray-100 flex items-center justify-center overflow-hidden"
